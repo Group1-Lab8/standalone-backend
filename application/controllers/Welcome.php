2dd3b4f75dc4f7b81f675b35c690e3022de3fd5c
Steven Tsao <steven.tsao777@gmail.com>
2017-04-06 17:29:03 -0700

    backend webapp setup

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,24 +6,10 @@ class Welcome extends Application {
 
     public function index() {
 
-        //$this->load->helper('formfields');
-        $this->data['title'] = 'Ferry Trip Planner (Server)';
-        $this->data['pagebody'] = 'welcome_message';
-        $this->render();
-
-        /* $result = '';
-          $oddrow = true;
-          foreach ($this->categories->all() as $category) {
-          //			$viewparms = array(
-          //				'direction' => ($oddrow ? 'left' : 'right')
-          //			);
-          //			$viewparms = array_merge($viewparms, $category);
-          $category->direction = ($oddrow ? 'left' : 'right');
-          $result .= $this->parser->parse('category-home', $category, true);
-          $oddrow = !$oddrow;
-          }
-          $this->data['content'] = $result;
-          $this->render(); */
+        echo "hello world from backend";
+        //$this->data['title'] = 'Ferry Trip Planner (Server)';
+        //$this->data['pagebody'] = 'welcome_message';
+        //$this->render();
     }
 
 }
